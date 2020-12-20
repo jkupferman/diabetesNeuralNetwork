101a1021e56fb6109e78e419a1f262c2ce65bd81
Jonathan Kupferman <jmkupferman@gmail.com>
2020-12-19 19:05:59 -0800

    Add example for how to make predictions

---
+++ make_predictions.py
@@ -0,0 +1,9 @@
+from keras.models import load_model
+import numpy as np
+
+model = load_model('weights.h5')
+
+values = np.array([[1,93,70,31,0,30.4,0.315,23]])
+
+print(values)
+print(model.predict(values))
