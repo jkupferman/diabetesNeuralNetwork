2d29e0c2dd5454495d616f8b69fc5c7c5eb608f2
Jonathan Kupferman <jmkupferman@gmail.com>
2020-12-19 19:17:43 -0800

    Use autopep8 to clean up code style

--- make_predictions.py
+++ make_predictions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 model = load_model('weights.h5')
 
-values = np.array([[1,93,70,31,0,30.4,0.315,23]])
+values = np.array([[1, 93, 70, 31, 0, 30.4, 0.315, 23]])
 
 print(values)
 print(model.predict(values))
